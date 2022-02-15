c56e2367a9e658419a8ab54dccd2916f85a22912
Mac <mac@Macs-MacBook-Pro.local>
2022-02-15 16:29:42 +0545

    project setup done

--- app/src/main/java/com/iions/appname/feature/main/screens/MainActivity.kt
+++ app/src/main/java/com/iions/appname/feature/main/screens/MainActivity.kt
@@ -3,7 +3,6 @@ package com.iions.appname.feature.main.screens
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.Gravity
 import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -142,7 +141,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(),
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         binding.navigationView.setNavigationItemSelectedListener(this)
-//        binding.drawerLayout.useCustomBehavior(Gravity.START)
     }
 
 }
