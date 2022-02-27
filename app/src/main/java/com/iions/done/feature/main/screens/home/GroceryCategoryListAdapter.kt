d889102a0e98c792019fcbdd58c5a766bb1540fb
Mac <mac@Macs-MacBook-Pro.local>
2022-02-27 14:28:02 +0545

    grocery list view implemented

--- app/src/main/java/com/iions/done/feature/main/screens/home/GroceryCategoryListAdapter.kt
+++ app/src/main/java/com/iions/done/feature/main/screens/home/GroceryCategoryListAdapter.kt
@@ -1,6 +1,7 @@
 package com.iions.done.feature.main.screens.home
 
 import android.annotation.SuppressLint
+import androidx.core.content.ContextCompat
 import androidx.databinding.ViewDataBinding
 import com.iions.done.R
 import com.iions.done.base.BaseAdapter
@@ -12,6 +13,8 @@ class GroceryCategoryListAdapter(
     private var dataList: MutableList<GroceryCategoryResponse>,
     private val onItemSelectedListener: (GroceryCategoryResponse) -> Unit
 ) : BaseAdapter<GroceryCategoryResponse, GroceryCategoryListAdapter.GroceryCategoryListViewHolder>() {
+
+    private var mCheckedPostion = -1
 
     override fun getViewHolder(
         binding: ViewDataBinding,
@@ -43,9 +46,53 @@ class GroceryCategoryListAdapter(
         override fun bindView(obj: GroceryCategoryResponse) {
             super.bindView(obj)
             binding.tvName.text = obj.name
+
+            if (mCheckedPostion == -1) {
+                setSelectedItem(binding, false)
+            } else {
+                if (mCheckedPostion == absoluteAdapterPosition) {
+                    setSelectedItem(binding, true)
+                } else {
+                    setSelectedItem(binding, false)
+                }
+            }
             binding.constraint.setOnClickListener {
+                setSelectedItem(binding, true)
+                if (mCheckedPostion != absoluteAdapterPosition) {
+                    setSelectedItem(binding, true)
+                    notifyItemChanged(mCheckedPostion)
+                    mCheckedPostion = absoluteAdapterPosition
+                }
                 onItemSelectedListener(obj)
             }
+        }
+    }
+
+    private fun setSelectedItem(binding: ItemCategoryBinding, isSelected: Boolean) {
+        if (isSelected) {
+            binding.constraint.background =
+                ContextCompat.getDrawable(
+                    binding.root.context,
+                    R.drawable.bg_category_selected
+                )
+            binding.tvName.setTextColor(
+                ContextCompat.getColor(
+                    binding.root.context,
+                    R.color.white_FFFFFF
+                )
+            )
+        } else {
+            binding.constraint.background =
+                ContextCompat.getDrawable(
+                    binding.root.context,
+                    R.drawable.bg_category
+                )
+            binding.tvName.setTextColor(
+                ContextCompat.getColor(
+                    binding.root.context,
+                    R.color.black_000000
+                )
+            )
         }
     }
 }
